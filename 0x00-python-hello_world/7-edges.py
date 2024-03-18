d72a2a492100f407fc90a1ff7892acd0d60b9b76
mazi-samuel <mazizobo@gmail.com>
2024-03-18 14:58:22 +0100

    stire_fry

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -2,7 +2,7 @@
 word = "Holberton"
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
 word_first_3 = word[:3]
-word_last_2 = word[8:9]
+word_last_2 = word[7:9]
 middle_word = word[1:8]
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
