f298558358bcb064870b04a149cb05985354f665
mazi-samuel <mazizobo@gmail.com>
2024-03-24 15:22:27 +0100

    still on it

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 # Printing the alphabet using a loop
-for char in range(ord('a'), ord('z')+1):
-        print(chr(char), end="")
+print(''.join([f"{chr(i)}" for i in range(97, 123)]))
